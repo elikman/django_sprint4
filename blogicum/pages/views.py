a77bf31ae8acea4b01a71c9e560b0cd35bcbcd27
Eltaj Nabiev <eltajnabiev464@gmail.com>
2024-05-18 23:17:50 +0300

    Описание ваших изменений

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,32 +1,13 @@
-from http import HTTPStatus
-
 from django.shortcuts import render
-from django.views.generic import TemplateView
 
 
 def csrf_failure(request, reason=''):
-    """Ошибка CSRF токена."""
-    return render(request, 'pages/403csrf.html', status=HTTPStatus.FORBIDDEN)
+    return render(request, 'pages/403csrf.html', status=403)
 
 
 def page_not_found(request, exception):
-    """Страница не найдена."""
-    return render(request, 'pages/404.html', status=HTTPStatus.NOT_FOUND)
+    return render(request, 'pages/404.html', status=404)
 
 
 def server_error(request):
-    """Ошибка сервера."""
-    return render(request, 'pages/500.html',
-                  status=HTTPStatus.INTERNAL_SERVER_ERROR)
-
-
-class AboutPageView(TemplateView):
-    """О проекте."""
-
-    template_name = 'pages/about.html'
-
-
-class Rules(TemplateView):
-    """Наши правила."""
-
-    template_name = 'pages/rules.html'
+    return render(request, 'pages/500.html', status=500)
