50590f91e3c00d8edf7063e2ddf8f30ef9ef8370
elikman <46077043+elikman@users.noreply.github.com>
2024-05-19 23:39:14 +0300

    Update views.py

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -7,8 +7,8 @@ def csrf_failure(request, reason=''):
 
 
 def page_not_found(request, exception):
-    return render(request, 'pages/404.html', status=404)
+    return render(request, 'pages/404.html', status=HTTPStatus.NOT_FOUND)
 
 
 def server_error(request):
-    return render(request, 'pages/500.html', status=500)
+    return render(request, 'pages/500.html', status=HTTPStatus.INTERNAL_SERVER_ERROR)
